fd729f68c806781dfa4afe6b7888a8c6ec8fa41b
FelipeRodriguees <contactfeliper@gmail.com>
2022-06-29 08:51:27 -0300

    init

---
+++ book.py
@@ -0,0 +1,49 @@
+
+import random
+
+class Book:
+
+    def __init__(self, title, author, year, publisher, number_of_copiesa_vailable, publication_date):
+        self.__id = random.randint(1, 1000),
+        self.__title = title,
+        self.__author = author,
+        self.__year = year,
+        self.__publisher = publisher,
+        self.__number_of_copiesa_vailable = number_of_copiesa_vailable,
+        self.__publication_date = publication_date
+
+    def set_title(self, title):
+        self.__title = title
+
+    def get_title(self):
+        return self.__title
+
+    def set_author(self, author):
+        self.__author = author
+
+    def get_author(self):
+        return self.__author
+
+    def set_year(self, year):
+        self.__year = year
+
+    def get_year(self):
+        return self.__year
+
+    def set_publisher(self, publisher):
+        self.__publisher = publisher
+
+    def get_publisher(self):
+        return self.__publisher
+
+    def set_number_of_copiesa_vailable(self, number_of_copiesa_vailable):
+        self.__number_of_copiesa_vailable = number_of_copiesa_vailable
+
+    def get_number_of_copiesa_vailable(self):
+        return self.__number_of_copiesa_vailable
+
+    def set_publication_date(self, publication_date):
+        self.__publication_date = publication_date
+
+    def get_publication_date(self):
+        return self.__publication_date
---
+++ models/book.py
@@ -0,0 +1,13 @@
+# Livros: Defina uma classe Python com métodos para fazer o seguinte:
+
+
+# 4. A classe deve incluir verificação de erros (por exemplo, tratamento de exceções).
+# 5. A aula deve ser documentada por comentários.
+from book import Book
+
+
+def add_book():
+
+    book = Book("312", "Felipe", 2022, "Test", 21312, "10/01/2022")
+
+    print(book.set_title(book, "sadasdsa"))
